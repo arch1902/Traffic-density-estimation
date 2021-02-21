23a8232e700da05eef61234f818459e57a543243
Aman Gupta <amangupt01@gmail.com>
2021-02-22 01:35:51 +0530

    adding crude code

--- subtask1.cpp
+++ subtask1.cpp
@@ -1,61 +1,77 @@
 #include "opencv2/opencv.hpp" 
+#include<string>
 
 using namespace cv;
 using namespace std;
 
 vector<Point2f> corners1, corners2;
+Mat gray_image;
+Mat display;
+Mat im_src;
+void perspective();
+int n ;
+
+
+int resolution (int x){
+    return (x*2)/3;
+}
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-     if  ( event == EVENT_LBUTTONDOWN || event == EVENT_RBUTTONDOWN || event == EVENT_MBUTTONDOWN) {
+    imshow("My Window", display);
+     if  ( n<4 && (event == EVENT_LBUTTONDOWN || event == EVENT_RBUTTONDOWN || event == EVENT_MBUTTONDOWN) ) {
 	 			corners1.push_back(Point2f(x, y));
+                n+=1;
+                String to_display = "("+to_string(x)+","+to_string(y)+")";
+                putText(display,to_display,Point2f(x,y),FONT_HERSHEY_SIMPLEX,1, (0,255,0),3);
+                imshow("My Window", display);
 				cout<<x<<" "<<y<<endl;
+        if (n==4){perspective();}
 	 }
 }
 
-int main( int argc, char** argv)
-{
-    // Read source image.
-    Mat im_src = imread("empty.jpg");
-	Mat gray_image;
-	cvtColor( im_src, gray_image, COLOR_BGR2GRAY );
-	
-    // Four corners of the book in source image
-	//Create a window
-     namedWindow("My Window", 1);
-
-     //set the callback function for any mouse event
-     setMouseCallback("My Window", CallBackFunc, NULL);
-
-     //show the image
-     imshow("My Window", gray_image);
-
-    // Wait until user press some key
-     waitKey(0);
+void perspective(){
 
     // Four corners in destination image.
-    corners2.push_back(Point2f(472,52));
-    corners2.push_back(Point2f(472, 830));
-    corners2.push_back(Point2f(800, 830));
-    corners2.push_back(Point2f(800, 52));
+    corners2.push_back(Point2f(resolution(472),resolution(52)));
+    corners2.push_back(Point2f(resolution(472),resolution(830)));
+    corners2.push_back(Point2f(resolution(800),resolution(830)));
+    corners2.push_back(Point2f(resolution(800),resolution(52)));
 
     // Calculate Homography
     Mat H = findHomography(corners1, corners2);
 
     // Warp source image to destination based on homography
     Mat img1_warp;
-    warpPerspective(gray_image, img1_warp, H, {1280,875});
-	cout<<im_src.size();
+    warpPerspective(gray_image, img1_warp, H, {resolution(1280),resolution(875)});
+	cout<<"Size of the source image is "<<im_src.size()<<"\n";
 
     // Display images
-    imshow("Source Image", gray_image);
-    imshow("Warped Source Image", img1_warp);
+    //imshow("Source Image", gray_image);
+    imshow("Perspective Image", img1_warp);
+    imwrite("Perspective Image.jpg",img1_warp);
 
-    waitKey(0);
-
-	Mat cropedImage = img1_warp(Rect(472,52,329,779));
+	Mat cropedImage = img1_warp(Rect(resolution(472),resolution(52),resolution(329),resolution(779)));
 	imshow("Cropped Image", cropedImage);
+    imwrite("Cropped Image.jpg",cropedImage);
+}
 
+int main( int argc, char** argv)
+{
+    // Read source image.
+    im_src = imread("Resized_empty.jpg");
+	cvtColor( im_src, gray_image, COLOR_BGR2GRAY );
+	display = gray_image.clone();
+    // Four corners of the book in source image
+	//Create a window
+    namedWindow("My Window",1); 
+     cout<<"Please select four points on the image : \n";
+     //set the callback function for any mouse event
+    setMouseCallback("My Window", CallBackFunc, NULL);
+
+     //show the image
+    // Wait until user press some key
 	waitKey(0);
-
+    cout << "End of Programme\n";
+    
 }
